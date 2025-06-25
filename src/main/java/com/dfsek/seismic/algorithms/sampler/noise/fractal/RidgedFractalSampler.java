585f8de1d6dd6ad2f690662b7176ee0e41573226
Zoë Gidiere <duplexsys@protonmail.com>
2025-06-24 20:17:03 -0600

    Add Ridged Fractal derivative

--- src/main/java/com/dfsek/seismic/algorithms/sampler/noise/fractal/RidgedFractalSampler.java
+++ src/main/java/com/dfsek/seismic/algorithms/sampler/noise/fractal/RidgedFractalSampler.java
@@ -8,6 +8,7 @@
 package com.dfsek.seismic.algorithms.sampler.noise.fractal;
 
 import com.dfsek.seismic.math.numericanalysis.interpolation.InterpolationFunctions;
+import com.dfsek.seismic.type.sampler.DerivativeSampler;
 import com.dfsek.seismic.type.sampler.Sampler;
 
 
@@ -49,6 +50,76 @@ public class RidgedFractalSampler extends FractalNoiseFunction {
             y *= lacunarity;
             z *= lacunarity;
             amp *= gain;
+        }
+
+        return sum;
+    }
+
+
+    @Override
+    public boolean isDifferentiable() {
+        return DerivativeSampler.isDifferentiable(input);
+    }
+
+    @Override
+    public double[] getNoiseDerivativeRaw(long seed, double x, double y) {
+        double[] sum = {0, 0, 0};
+        double amp = fractalBounding;
+        double negative2Amp = amp * -2;
+
+        for(int i = 0; i < octaves; i++) {
+            // This should only be called after `input` is verified as a `DerivativeNoiseSampler`
+            // so this should be a safe cast
+            double[] noise = ((DerivativeSampler) input).getSampleDerivative(seed++, x, y);
+            noise[0] = Math.abs(noise[0]);
+            noise[1] = Math.abs(noise[1]);
+            noise[2] = Math.abs(noise[2]);
+
+            sum[0] += (noise[0] * -2 + 1) * amp;
+
+            // Directional derivative of each octave can be subject to the same addition and product
+            // as per derivative sum and product rules in order to produce the correct final derivative
+            sum[1] += noise[1] * negative2Amp;
+            sum[2] += noise[2] * negative2Amp;
+
+            amp *= InterpolationFunctions.lerp(1.0, 1 - noise[0], weightedStrength);
+
+            x *= lacunarity;
+            y *= lacunarity;
+            amp *= gain;
+            negative2Amp = amp * -2;
+        }
+
+        return sum;
+    }
+
+    @Override
+    public double[] getNoiseDerivativeRaw(long seed, double x, double y, double z) {
+        double[] sum = {0, 0, 0, 0};
+        double amp = fractalBounding;
+        double negative2Amp = amp * -2;
+
+        for(int i = 0; i < octaves; i++) {
+            double[] noise = ((DerivativeSampler) input).getSampleDerivative(seed++, x, y, z);
+            noise[0] = Math.abs(noise[0]);
+            noise[1] = Math.abs(noise[1]);
+            noise[2] = Math.abs(noise[2]);
+            noise[3] = Math.abs(noise[3]);
+
+            sum[0] += (noise[0] * -2 + 1) * amp;
+
+            // See comment in 2D version
+            sum[1] += noise[1] * negative2Amp;
+            sum[2] += noise[2] * negative2Amp;
+            sum[3] += noise[3] * negative2Amp;
+
+            amp *= InterpolationFunctions.lerp(1.0, 1 - noise[0], weightedStrength);
+
+            x *= lacunarity;
+            y *= lacunarity;
+            z *= lacunarity;
+            amp *= gain;
+            negative2Amp = amp * -2;
         }
 
         return sum;
